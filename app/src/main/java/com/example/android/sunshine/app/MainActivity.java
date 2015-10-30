eb86e5bfeca9a88269aece9b691d57acbe69fd0b
copanote <copanote@gmail.com>
2015-10-30 15:26:43 +0900

    Move BT module 2  - from ComBlueTooth.java to MainActivity.java of sunshine

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -15,13 +15,21 @@
  */
 package com.example.android.sunshine.app;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.android.sunshine.app.bt.TBlue;
 import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
 
 public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback {
@@ -34,6 +42,14 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
     //dongwook2.shin
     private String mWeather;
 
+    //move from ComBlueTooth.java to MainActivity.java of sunshine
+    // start of move
+    static TBlue tBlue;
+    byte[] LED_COLOR = new byte[5];
+    int idx = 0;
+    // end of move
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +57,23 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         mLocation = Utility.getPreferredLocation(this);
         mWeather = Utility.getPreferredRainbow(this);
         // Toast.makeText(getBaseContext(), mWeather, Toast.LENGTH_LONG).show();
+
+        //move from ComBlueTooth.java to MainActivity.java of sunshine
+        // start of move
+        LED_COLOR[0] = 0x01;    // blue
+        LED_COLOR[1] = 0x02;    // Green
+        LED_COLOR[2] = 0x04;    // RED
+        LED_COLOR[3] = 0x08;    // YELLOW
+        LED_COLOR[4] = 0x10;    // WHITE
+
+        IntentFilter filter1 = new IntentFilter(BluetoothDevice.ACTION_ACL_CONNECTED);
+        IntentFilter filter2 = new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED);
+        IntentFilter filter3 = new IntentFilter(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+        this.registerReceiver(BTReceiver, filter1);
+        this.registerReceiver(BTReceiver, filter2);
+        this.registerReceiver(BTReceiver, filter3);
+        this.getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        // end of move
 
 
 
@@ -68,6 +101,10 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         forecastFragment.setUseTodayLayout(!mTwoPane);
 
         SunshineSyncAdapter.initializeSyncAdapter(this);
+
+
+
+
     }
 
     @Override
@@ -134,4 +171,105 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
             startActivity(intent);
         }
     }
+
+    //move from ComBlueTooth.java to MainActivity.java of sunshine
+    // start of move
+
+    //The BroadcastReceiver that listens for bluetooth broadcasts
+    private final BroadcastReceiver BTReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+                //Do something if connected
+                Toast.makeText(getApplicationContext(), "flone connected", Toast.LENGTH_SHORT).show();
+                // floneConnected = true;
+            } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                //Do something if disconnected
+                Toast.makeText(getApplicationContext(), "flone disconnected", Toast.LENGTH_SHORT).show();
+                //floneConnected = false;
+                try {
+                    tBlue.close();
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "flone disconnected error", Toast.LENGTH_SHORT).show();
+                }
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                //Done searching
+                Toast.makeText(getApplicationContext(), "Bluetooth discovery finished.", Toast.LENGTH_SHORT).show();
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //Device found
+                Toast.makeText(getApplicationContext(), "Found Bluetooth device.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+
+    public void onDestroy() {
+        this.unregisterReceiver(BTReceiver);
+        super.onDestroy();
+    }
+
+    public void onStart() {
+        super.onStart();
+
+        tBlue = new TBlue(this);
+    }
+
+    public void onStop() {
+        if (tBlue!= null)
+            tBlue.close();
+        super.onStop();
+    }
+
+    public void onConnect(View view) {
+        Toast.makeText(this, "onConnect", Toast.LENGTH_SHORT).show();
+       /* if (!blueName.getText().toString().equals(""))
+            bluetoothId = blueName.getText().toString(); */
+        try {
+            tBlue.connect();
+        }
+        catch (NullPointerException ex) {
+            Toast.makeText(this, "Warning: Flone not connected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void onSend(View view) {
+        if (btConnected()) {
+            Toast.makeText(this, "isConnect == true", Toast.LENGTH_SHORT).show();
+            byte[] command = new byte[2];
+            command[0] = 0x16;
+            command[1] = LED_COLOR[idx++];
+            if (idx % 5 == 0) {
+                idx = 0;
+            }
+            tBlue.write(command);
+        } else {
+            Toast.makeText(this, "isConnect == false", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onRead(View view) {
+        if (btConnected()) {
+            Toast.makeText(this, "isConnect == true", Toast.LENGTH_SHORT).show();
+            //textView.setText(tBlue.read());
+        } else {
+            Toast.makeText(this, "isConnect == false", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean btConnected() {
+        boolean btConnected = false;
+        try {
+            btConnected = tBlue.streaming();
+        }
+        catch(Exception ex) {
+            btConnected = false;
+        }
+        return btConnected;
+    }
+
+    // end of move
 }
